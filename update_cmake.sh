0d8867c6daf5179d33e25cfddd7029a7101e426e
KitsuneAlex <alexh@karmakrafts.dev>
2023-08-06 01:19:10 +0200

    Update CMake submodule to latest commit

--- update_cmake.sh
+++ update_cmake.sh
@@ -5,14 +5,23 @@
 # @since 06/08/2023
 
 export PREV_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git checkout develop
+
+if [ `git rev-parse --verify develop 2>/dev/null` ]; then
+	export HAS_DEVELOP=true
+	git checkout develop
+fi
+
 cd cmake
 git pull
 cd ..
 git add -A
 git commit -m "Update CMake submodule to latest commit"
 git push
-git checkout master
-git merge develop
-git push
+
+if [ "$HAS_DEVELOP" = true ]; then
+	git checkout master
+	git merge develop
+	git push
+fi
+
 git checkout $PREV_BRANCH
